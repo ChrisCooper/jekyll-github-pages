5fc6b864e327e57792591490dfbf83ef222ef7d3
Chris Cooper <chris@quixotic-chris.com>
2018-03-13 00:30:08 -0400

    Make windows poll for changes

--- jekyll-serve.ps1
+++ jekyll-serve.ps1
@@ -5,4 +5,4 @@ $SCRIPT_DIRECTORY = (get-item $SCRIPT_FULL_PATH ).Directory.FullName
 $PROJECT_DIR = $SCRIPT_DIRECTORY.Replace("\", "/")
 Write-Output "Project directory is $PROJECT_DIR"
 
-docker run --rm -v ${PROJECT_DIR}:/site:Z -it -p 4000:4000 $IMAGE_NAME bundle exec jekyll serve --host 0.0.0.0 --watch --verbose
+docker run --rm -v ${PROJECT_DIR}:/site:Z -it -p 4000:4000 $IMAGE_NAME bundle exec jekyll serve --host 0.0.0.0 --force_polling --verbose
